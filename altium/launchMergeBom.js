1d19c2b0a89b3b3c4f0e682a64045b81c7070fa2
Daniele Basile <asterix24@gmail.com>
2015-12-28 08:02:37 +0100

    Add license.

--- altium/launchMergeBom.js
+++ altium/launchMergeBom.js
@@ -1,3 +1,20 @@
+ /* MergeBom is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Copyright 2015 Daniele Basile <asterix24@gmail.com>
+ */
+
 function dirname(path) {
      return path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');
 }
@@ -25,22 +42,3 @@ function ExeMergeBom() {
         }
     }
 }
-
-function Prova() {
-   var w = GetWorkspace();
-   var f = OpenFile("cioa");
-   showMessage("ciao");
-   /*
-   if (Client) {
-      var report_doc = Client.OpenDocument('Text', "prova");
-      if (report_doc) {
-         Client.ShowDocument(report_doc);
-      }
-   }
-   */
-   //for (i = 0; i < wk.DM_ProjectCount; i++) {
-//var p = wk.DM_Projects(i);
-//}
-
-}
-
