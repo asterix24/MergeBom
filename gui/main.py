b9a23209de4a16fe59dc62f6374cc0be01e34bcd
Daniele Basile <asterix24@gmail.com>
2019-11-24 16:31:05 +0100

    gui: draw first mockup.

--- gui/main.py
+++ gui/main.py
@@ -1,5 +1,11 @@
-from PyQt5.QtCore import QDateTime, Qt, QTimer
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+from PyQt5.QtCore import QDateTime, Qt, QAbstractTableModel, QVariant
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit, QTableView,
                              QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
                              QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
                              QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
@@ -7,8 +13,6 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
-
-import sys
 
 
 class MergeBomGUI(QDialog):
@@ -20,24 +24,27 @@ class MergeBomGUI(QDialog):
     def __init__(self, parent=None):
         super(MergeBomGUI, self).__init__(parent)
 
-        styleComboBox = QComboBox()
-        styleComboBox.addItems(QStyleFactory.keys())
+        self.source_data = None
+        self.src_data = None
+        self.src_data_name = None
+        self.src_data_name_lbl = None
 
-        styleLabel = QLabel("&Style:")
-        styleLabel.setBuddy(styleComboBox)
+        self.altium_group = None
+        self.wk_path = None
 
-        defaultPushButton = QPushButton("Default Push Button")
-        defaultPushButton.setDefault(True)
-        defaultPushButton.clicked.connect(self.__on_click)
+        self.param_data = None
+        self.headers = ["Name", "Value"]
+        self.rows = [('-', '-')]
 
-        topLayout = QHBoxLayout()
-        topLayout.addWidget(styleLabel)
-        topLayout.addWidget(styleComboBox)
-        topLayout.addWidget(defaultPushButton)
+        self.__altium_group()
+        self.__param_data()
+        self.__source_data()
 
-        mainLayout = QGridLayout()
-        mainLayout.addLayout(topLayout, 0, 0, 1, 2)
-        self.setLayout(mainLayout)
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.altium_group)
+        main_layout.addWidget(self.source_data)
+        main_layout.addWidget(self.param_data)
+        self.setLayout(main_layout)
 
         self.setWindowTitle("Merge BOM GUI")
         QApplication.setStyle(QStyleFactory.create('Fusion'))
@@ -45,12 +52,108 @@ class MergeBomGUI(QDialog):
 
     @pyqtSlot()
     def __on_click(self):
-        app = FileDialog()
-        print(app.selection)
+        app = FileDialog(rootpath=self.wk_path.text())
+        line = app.selection()
+        print(line)
+        if line is not None:
+            self.wk_path.setText(line)
+
+            root, ext = os.path.splitext(
+                os.path.basename(line))
+
+            lbl_root = "Workspace Name:"
+            data_type = "Altium Workspace"
+            if ext in "PrjPCB":
+                data_type = "Altium Project"
+                lbl_root = "Project Name:"
+
+            self.src_data.insertItem(0, data_type)
+            self.src_data.setCurrentIndex(0)
+
+            self.src_data_name.setText(root)
+            self.src_data_name_lbl.setText(lbl_root)
+
+    def __source_data(self):
+        self.source_data = QGroupBox("Source Data")
+
+        self.src_data = QComboBox()
+        self.src_data.addItems(["Custom"])
+        srclbl = QLabel("&From:")
+        srclbl.setBuddy(self.src_data)
+
+        hsrc = QHBoxLayout()
+        hsrc.addWidget(srclbl)
+        hsrc.addWidget(self.src_data)
+
+        self.src_data_name = QLabel("None")
+        self.src_data_name_lbl = QLabel("Data Name:")
+
+        info = QHBoxLayout()
+        info.addWidget(self.src_data_name_lbl)
+        info.addWidget(self.src_data_name)
+
+        data = QVBoxLayout()
+        data.addLayout(hsrc)
+        data.addLayout(info)
+
+        self.source_data.setLayout(data)
+
+    def __param_data(self):
+        self.param_data = QGroupBox("Parms")
+        model = ParamDataModel(self.headers, self.rows)
+        view = QTableView()
+        view.setModel(model)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(view)
+
+        self.param_data.setLayout(hbox)
+
+    def __altium_group(self):
+        self.altium_group = QGroupBox("Select Altium Wks or Prj")
+
+        self.wk_path = QLineEdit(os.path.expanduser('~/'))
+        btn = QPushButton("Select")
+        btn.setDefault(True)
+        btn.clicked.connect(self.__on_click)
+
+        lfile = QHBoxLayout()
+        lfile.addWidget(self.wk_path)
+        lfile.addWidget(btn)
+
+        self.altium_group.setLayout(lfile)
+
+
+class ParamDataModel(QAbstractTableModel):
+    """
+    Model to display all mergebom param get from variuos source,
+    like altium wk/prj or use insertion.
+    """
+
+    def __init__(self, headers=["Name", "Value"], rows=[("-", "-")]):
+        super().__init__()
+        self.rows = rows
+        self.headers = headers
+
+    def rowCount(self, parent):
+        return len(self.rows)
+
+    def columnCount(self, parent):
+        return len(self.headers)
+
+    def data(self, index, role):
+        if role != Qt.DisplayRole:
+            return QVariant()
+        return self.rows[index.row()][index.column()]
+
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole or orientation != Qt.Horizontal:
+            return QVariant()
+        return self.headers[section]
 
 
 FILE_FILTERS = {
-    'workspace': "Altium WorkSpace (*.DsnWrk);;Altium Project (*.PrjPcb);;All Files (*)"
+    'workspace': "Altium WorkSpace (*.DsnWrk);;Altium Project (*.PrjPCB);;All Files (*)"
 }
 
 
@@ -59,11 +162,12 @@ class FileDialog(QWidget):
     Common Dialog to select file from filesystem
     """
 
-    def __init__(self, title="Open File", ext_filter="workspace", mode='open'):
+    def __init__(self, title="Open File", ext_filter="workspace", mode='open', rootpath=os.path.expanduser("~")):
         super().__init__()
         self.title = title
         self.mode = mode
         self.filter = ext_filter
+        self.rootpath = rootpath
 
         self.file_path = None
 
@@ -97,7 +201,7 @@ class FileDialog(QWidget):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         filename, _ = QFileDialog.getOpenFileName(
-            self, self.title, "", FILE_FILTERS[self.filter], options=options)
+            self, self.title, self.rootpath, FILE_FILTERS[self.filter], options=options)
         if filename:
             self.file_path = filename
 
@@ -105,7 +209,7 @@ class FileDialog(QWidget):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         files, _ = QFileDialog.getOpenFileNames(
-            self, self.title, "", FILE_FILTERS[self.filter], options=options)
+            self, self.title, self.rootpath, FILE_FILTERS[self.filter], options=options)
         if files:
             self.file_path = files
 
@@ -113,7 +217,7 @@ class FileDialog(QWidget):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         filename, _ = QFileDialog.getSaveFileName(
-            self, self.title, "", FILE_FILTERS[self.filter], options=options)
+            self, self.title, self.rootpath, FILE_FILTERS[self.filter], options=options)
         if filename:
             self.file_path = filename
 
